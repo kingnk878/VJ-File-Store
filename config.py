46841a9669bb7882de98235a17890cc0150071da
kingnk878 <nishantlakhwa69@gmail.com>
2025-08-22 04:23:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,11 +32,11 @@ CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 
 # If Clone Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://nishantlakhwa68:eWeLCbQoYIVXyviS@cluster0.1el2wcz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-CDB_NAME = environ.get("CDB_NAME", "Cluster0")
+CDB_NAME = environ.get("CDB_NAME", "Clonetechvj")
 
 # Database Information
 DB_URI = environ.get("DB_URI", "mongodb+srv://nishantlakhwa69:0TErWi8redvAHzeH@cluster1.r0njnjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1")
-DB_NAME = environ.get("DB_NAME", "Cluster1")
+DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
